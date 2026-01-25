cbbece1d3b7664c8748a54cb6bdc1878ec18ecce
Darren Daniel <darren.d.atmj@gmail.com>
2026-01-24 19:13:18 -0800

    Added image for computer lab page. Added apply for work page to group all work opportunities. Arrow now rotates when dropdown is toggled. Added dropdown on the sidebar. Reduced the max-width of the main container wrapper to 1280px.

--- js/navigation.js
+++ js/navigation.js
@@ -2,8 +2,13 @@ const sidebarBtn = document.getElementById("sidebar-btn");
 const sidebar = document.getElementById("sidebar");
 const sidebarExit = document.getElementById("sidebar-exit-btn");
 const navDropdownBtn = document.getElementById("nav-dropdown-btn");
+const navDropdownArrow = document.getElementById("nav-dropdown-arrow");
 const navDropdownContent = document.getElementById("nav-dropdown-content");
+const sidebarDropdownBtn = document.getElementById("sidebar-dropdown-btn");
+const sidebarDropdownArrow = document.getElementById("sidebar-dropdown-arrow");
+const sidebarDropdownContent = document.getElementById("sidebar-dropdown-content");
 const overlay = document.getElementById("overlay");
+const mainContent = document.getElementById("main-content");
 
 sidebarBtn.addEventListener('click', () => {
     sidebar.classList.toggle('open');
@@ -17,12 +22,26 @@ sidebarExit.addEventListener('click', () => {
 
 navDropdownBtn.addEventListener('click', () => {
     navDropdownContent.classList.toggle('open');
+    navDropdownArrow.classList.toggle('open');
 });
+
+sidebarDropdownBtn.addEventListener('click', () => {
+    sidebarDropdownContent.classList.toggle('open');
+    sidebarDropdownArrow.classList.toggle('open');
+})
 
 overlay.addEventListener('click', (e) => {
     if (!sidebar.contains(e.target) && !sidebarBtn.contains(e.target) &&
         sidebar.classList.contains('open')) {
         sidebar.classList.remove('open');
         overlay.classList.remove('active');
+    }
+});
+
+mainContent.addEventListener('click', (e) => {
+    if (!navDropdownContent.contains(e.target) && !navDropdownBtn.contains(e.target) &&
+    navDropdownContent.classList.contains('open')) {
+        navDropdownContent.classList.remove('open');
+        navDropdownArrow.classList.remove('open');
     }
 });
