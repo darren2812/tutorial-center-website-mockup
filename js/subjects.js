cfbd38c0b5d9346a38c9791cd503474e2ca6d47f
Darren Daniel <darren.d.atmj@gmail.com>
2026-03-11 17:13:32 -0700

    Filters are now working and schedules headers and content are re ndered based on the data in Google Apps Script.

--- js/subjects.js
+++ js/subjects.js
@@ -1,32 +1,51 @@
 async function loadSubject() {
   const params = new URLSearchParams(window.location.search);
   const subjectId = params.get("id");
-  const res = await fetch("https://script.google.com/macros/s/AKfycby2TLCtNSapumt7K_7kiJp0TUDyQh6s1v4ovZ1b46H8Tu6amAlVBYuckkAe43anlfu16g/exec?subject=${subjectId}");
+  const res = await fetch(`https://script.google.com/macros/s/AKfycby2TLCtNSapumt7K_7kiJp0TUDyQh6s1v4ovZ1b46H8Tu6amAlVBYuckkAe43anlfu16g/exec?subject=${subjectId}`);
   const subjects = await res.json();
   return subjects;
 }
 
 function createRow(entry) {
   const tableRow = document.createElement("tr");
-  tableRow.innerHTML = `
-  <td>${entry.Course.toUpperCase()}</td>
-  <td>${entry.Instructor}</td>
-  <td>${entry.Sunday}</td>
-  <td>${entry.Monday}</td>
-  <td>${entry.Tuesday}</td>
-  <td>${entry.Wednesday}</td>
-  <td>${entry.Thursday}</td>
-  <td>${entry.Friday}</td>
-  <td>${entry.Saturday}</td>
-`   ;
+  Object.entries(entry).forEach(([key, value]) => {
+    if (key != "Saturday") {
+      const tableData = document.createElement("td");
+      tableData.textContent = value;
+      tableRow.append(tableData);
+    }
+  });
   return tableRow;
 }
 
-function renderTable(data) {
-  const tableBody = document.getElementById("table-contents");
-  tableBody.innerHTML = "";
+// have to change this function because now we have multiple sections instead of just one schedule
+function renderTable(data, section) {
+  if (!data) {
+    document.getElementById(`${section}-info`).remove();
+    return;
+  }
 
+  const TABLE_CONFIG = {
+    dropin: ["Course", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",],
+    etc: ["Course", "Instructor", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    online: ["Course", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+  };
+
+  // creating headings
+  const tableHeading = document.getElementById(`${section}-table-headings`);
+  tableHeading.innerHTML = "";
+  TABLE_CONFIG[section].forEach(heading => {
+    const th = document.createElement("th");
+    th.scope = "col";
+    th.textContent = heading;
+    tableHeading.appendChild(th);
+  });
+
+  // creating table body
+  const tableBody = document.getElementById(`${section}-table-contents`);
+  tableBody.innerHTML = "";
   data.forEach(element => {
+    console.log(element);
     tableBody.appendChild(createRow(element));
   });
 }
@@ -36,26 +55,53 @@ function populateFilters(data) {
   const uniqueInstructors = new Set();
   const courseFilter = document.getElementById("course-filter");
   const instructorFilter = document.getElementById("instructor-filter");
+  const typeFilter = document.getElementById("type-filter");
 
-  data.forEach((dataEntry) => {
-    uniqueCourses.add(dataEntry.Course);
-    if (dataEntry.Instructor) {
-      uniqueInstructors.add(dataEntry.Instructor);
-    }
+  Object.values(data).forEach((section) => {
+    Object.values(section).forEach(dataEntry => {
+      uniqueCourses.add(dataEntry.Course);
+      if (dataEntry.Instructor) {
+        uniqueInstructors.add(dataEntry.Instructor);
+      }
+    });
   });
 
-  uniqueCourses.forEach((uniqueEntry) => {
+  const sortedCourses = [...uniqueCourses].sort((a, b) => a.localeCompare(b));
+  const sortedInstructors = [...uniqueInstructors].sort((a, b) => a.localeCompare(b));
+
+  sortedCourses.forEach((uniqueEntry) => {
     courseFilter.add(new Option(uniqueEntry, uniqueEntry.toLowerCase()));
   });
 
-  uniqueInstructors.forEach((uniqueEntry) => {
+  sortedInstructors.forEach((uniqueEntry) => {
     instructorFilter.add(new Option(uniqueEntry, uniqueEntry.toLowerCase()));
+  });
+
+  const TYPE_LABELS = {
+    dropin: "Drop-in Tutoring",
+    etc: "Extending the Class (ETC)",
+    online: "Online Tutoring"
+  };
+
+  Object.keys(data).forEach(type => {
+    const label = TYPE_LABELS[type] || type;
+    typeFilter.add(new Option(label, type));
   });
 }
 
 
 loadSubject().then(subjects => {
-  renderTable(subjects);
+  console.log(subjects);
+  const SECTIONS = ["dropin", "etc", "online"];
+  SECTIONS.forEach(section => {
+    if (subjects[section]) {
+      console.log(subjects[section]);
+      renderTable(subjects[section], section);
+    }
+    else {
+      document.getElementById(`${section}-info`).remove();
+    }
+  });
   populateFilters(subjects);
 });
 
@@ -110,8 +156,8 @@ fetch("https://script.google.com/macros/s/AKfycbyGjEIH9y0M7NNRb1mlWXtGNU8zFF-k1O
         iframe.src = section.iframe;
         iframe.style.height = `${section.height}px`;
 
-        if (sectionName === "drop-in") {
-          document.getElementById("drop-in-location").textContent = section?.["location"];
+        if (sectionName === "dropin") {
+          document.getElementById("dropin-location").textContent = section?.["location"];
           const dropInLocation = document.createElement("li");
           dropInLocation.textContent = `Drop in at ${section?.["location"]} during hours below.`;
           locationList.appendChild(dropInLocation);
@@ -143,7 +189,7 @@ fetch("https://script.google.com/macros/s/AKfycbyGjEIH9y0M7NNRb1mlWXtGNU8zFF-k1O
       document.getElementById("subject-description").remove();
     }
 
-    // applySection("drop-in", "drop-in-iframe", "drop-in-info");
+    // applySection("dropin", "dropin-iframe", "dropin-info");
     applySection("etc", "etc-iframe", "etc-info");
     // applySection("online", "online-iframe", "online-info");
 
